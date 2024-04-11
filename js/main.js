f9e2a4eeb9f8bd3b7dc7f60b6c1b13f4ea3144b1
Bulgaks <bulgaks92@gmail.com>
2024-04-11 16:44:14 +0300

    Добавляет редактор эффектов

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import { pictureCollections } from './data.js';
 import { addUserImage } from './minimize.js';
 import { initListener } from './fullscreen-picture.js';
 import './form.js';
+import './scale.js';
+import './effects.js';
 
 initListener(pictureCollections);
 addUserImage(pictureCollections);
